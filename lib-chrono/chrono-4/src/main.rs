435ce06030fdf16c5855ebbf82be9e30500360e0
cardigan1008 <211250058@smail.nju.edu.cn>
2024-01-31 14:05:52 +0800

    feat: chrono-4

---
+++ lib-chrono/chrono-4/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let _local0 = chrono::naive::NaiveDateTime::from_timestamp(-8377300, 742391807);
+    chrono::Datelike::with_month0(&(_local0), 4294967295);
+    chrono::Datelike::with_month(&(_local0), 4294967295);
+}
