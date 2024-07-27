80bb85792e2b4f285edeadbe0e04a31e992f6441
Sigger <1517158051@qq.com>
2024-07-28 00:13:50 +0800

    revert update proc-macro2-1

--- lib-proc-macro2/proc-macro2-1/src/main.rs
+++ lib-proc-macro2/proc-macro2-1/src/main.rs
@@ -1,13 +1,4 @@
 fn main() {
-    let input = "    𓀕 c    ";
-    let mut tokens = input
-        .parse::<proc_macro2::TokenStream>()
-        .unwrap()
-        .into_iter();
-
-    let ident1 = tokens.next().unwrap();
-    ident1.span().source_text().unwrap();
-
-    let ident2 = tokens.next().unwrap();
-    ident2.span().source_text().unwrap();
+    proc_macro2::Span::call_site();
+    println!("ran anyway");
 }
